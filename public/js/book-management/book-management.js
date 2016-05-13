df1c6f006095476abcfa9fa1d8513217c5016764
rayakkad <rayakkad@amplify.com>
2016-05-13 17:30:18 -0400

    Added checkbox

--- public/js/book-management/book-management.js
+++ public/js/book-management/book-management.js
@@ -1,3 +1,6 @@
 (function($, window, document, undefined) {
-    $('#book-results-table').DataTable();
+    $('#book-results-table').DataTable({
+        "lengthMenu": [[50, 100, 150, -1], [50, 100, 150, "All"]],
+        // "dom": 'iftlp'
+    });
 })(jQuery, window, document);
