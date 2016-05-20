8bf54ba3bff3dcb97f818218f3039a44bed7711d
rayakkad <rayakkad@amplify.com>
2016-05-20 13:36:49 -0400

    Changes to edit page layout

--- public/js/book-management/book-management.js
+++ public/js/book-management/book-management.js
@@ -1,6 +1,11 @@
 (function($, window, document, undefined) {
     $('#book-results-table').DataTable({
         "lengthMenu": [[50, 100, 150, -1], [50, 100, 150, "All"]],
+        "order": [],
+        "columnDefs": [ {
+            "targets"  : 'no-sort',
+            "orderable": false,
+        }]
         // "dom": 'iftlp'
     });
 })(jQuery, window, document);
