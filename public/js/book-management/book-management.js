fa4871040c86056420448b96fbbe98f13c738047
rayakkad <rayakkad@amplify.com>
2016-05-20 14:13:34 -0400

    Add all tabs

--- public/js/book-management/book-management.js
+++ public/js/book-management/book-management.js
@@ -6,6 +6,32 @@
             "targets"  : 'no-sort',
             "orderable": false,
         }]
-        // "dom": 'iftlp'
+    });
+
+    $('#personal-list-table').DataTable({
+        "lengthMenu": [[50, 100, 150, -1], [50, 100, 150, "All"]],
+        "order": [],
+        "columnDefs": [ {
+            "targets"  : 'no-sort',
+            "orderable": false,
+        }]
+    });
+
+    $('#school-list-table').DataTable({
+        "lengthMenu": [[50, 100, 150, -1], [50, 100, 150, "All"]],
+        "order": [],
+        "columnDefs": [ {
+            "targets"  : 'no-sort',
+            "orderable": false,
+        }]
+    });
+
+    $('#district-list-table').DataTable({
+        "lengthMenu": [[50, 100, 150, -1], [50, 100, 150, "All"]],
+        "order": [],
+        "columnDefs": [ {
+            "targets"  : 'no-sort',
+            "orderable": false,
+        }]
     });
 })(jQuery, window, document);
